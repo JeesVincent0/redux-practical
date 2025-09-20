f5f1fe98ae3cb2837ca17c4eec08cc13e57eb66a
Jees Vincent <jeesvincent0@gmail.com>
2025-09-20 21:10:08 +0530

    Initial SetUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <>
-    
-    </>
+    <div className="h-screen w-screen flex justify-center items-center bg-black/80">
+     <h1 className="font-bold text-7xl text-white cursor-pointer">Initial set up for redux practicals</h1>
+    </div>
   )
 }
 
