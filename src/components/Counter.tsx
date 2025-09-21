c9a69b094c2b62b750c570e4b3f24b89f8845ab2
Jees Vincent <jeesvincent0@gmail.com>
2025-09-21 16:37:07 +0530

    small changes in UI

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,26 @@
-import { style } from "./counterStyle";
+import { counterStyle } from "./counterStyle";
 import { increment, decremenet, reset } from "../redux/counterSlice";
-import {type RootState, type AppDispatch } from "../redux/store";
+import { type RootState, type AppDispatch } from "../redux/store";
 import { useSelector, useDispatch } from "react-redux";
 import React from "react";
 
 
 const Counter: React.FC = () => {
 
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch<AppDispatch>();
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch<AppDispatch>();
 
-  return (
-    <div className="absolute h-screen w-screen flex justify-center items-center bg-black/80">
-      <div className="relative h-[80vh] w-[25vw] rounded-2xl bg-white shadow-sm flex flex-col justify-center">
-        <p className={style.displayCounter}>Current Value {count}</p>
-        <button className={style.button} onClick={() => dispatch(increment())}>Increment by 1</button>
-        <button className={style.button} onClick={() => dispatch(decremenet())}>Decrement by 1</button>
-        <button className={style.button} onClick={() => dispatch(reset())}>Reset</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className={counterStyle.mainDiv}>
+            <div className={counterStyle.innerDiv}>
+                <p className={counterStyle.counterDisplay}>Redux-Practical</p>
+                <p className={counterStyle.counterDisplay}>Current Value {count}</p>
+                <button className={counterStyle.button} onClick={() => dispatch(increment())}>Increment by 1</button>
+                <button className={counterStyle.button} onClick={() => dispatch(decremenet())}>Decrement by 1</button>
+                <button className={counterStyle.button} onClick={() => dispatch(reset())}>Reset</button>
+            </div>
+        </div>
+    )
 }
 
 export default Counter
--- src/components/counterStyle.ts
+++ src/components/counterStyle.ts
@@ -1,4 +1,6 @@
-export const style = {
-    displayCounter: "h-10 w-[80%] flex justify-center items-center mx-auto bg-blue-500 mb-2 rounded-sm cursor-pointer hover:bg-blue-900 font-bold text-white",
-    button: "h-10 w-[80%] mx-auto bg-blue-500 mb-2 rounded-sm cursor-pointer hover:bg-blue-900 font-bold text-white"
+export const counterStyle = {
+    mainDiv: "bg-black/80 w-screen h-screen flex justify-center items-center", 
+    innerDiv: "w-[25vw] h-[80vh] bg-white/80 rounded-2xl flex flex-col items-center justify-center",
+    counterDisplay: "mb-4 font-bold text-black/60 text-center w-50 rounded-md p-2 m-0.5 border-4 border-black",
+    button: "bg-black/60 hover:bg-black/80 font-bold text-white w-50 rounded-md p-2 m-0.5",
 }
